1c41ac6c342cdb972cde6d809f27c1d6b4edf4f2
BogdanPasterak <bogdanpasterak@gmail.com>
2019-02-25 19:29:27 +0000

    JFileChooser (*.xml)

--- src/reportJUnitJava/Program.java
+++ src/reportJUnitJava/Program.java
@@ -3,21 +3,25 @@ package reportJUnitJava;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.*;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Program extends JFrame implements ActionListener {
 	JMenuBar menuBar;
 	JMenu file;
 	JMenuItem load;
 	JFileChooser fileChooser;
+	FileNameExtensionFilter fileFilter;
 	JLabel label;
 	
 
@@ -46,17 +50,22 @@ public class Program extends JFrame implements ActionListener {
 		setJMenuBar(menuBar);
 		
 		fileChooser = new JFileChooser();
+		fileFilter = new FileNameExtensionFilter("XML Files", "xml");
+		fileChooser.addChoosableFileFilter(fileFilter);
+		fileChooser.setAcceptAllFileFilterUsed(false);
+		
+		
 		
 		label = new JLabel("tu");
-		label.setBounds(20, 30, 400, 55);
+		label.setBounds(20, 30, 500, 55);
 		add(label);
 		
-		JButton button = new JButton("click");
-		button.setBounds(165, 135, 115, 55);
-		button.addActionListener(this);
-
-		// adding button on frame
-		add(button);
+//		JButton button = new JButton("click");
+//		button.setBounds(165, 135, 115, 55);
+//		button.addActionListener(this);
+//
+//		// adding button on frame
+//		add(button);
 		setVisible(true);
 
 	}
@@ -69,7 +78,13 @@ public class Program extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == load){
 			Integer returnVal = fileChooser.showOpenDialog(this);
-			label.setText("Laduje " + returnVal.toString());
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+	            File file = fileChooser.getSelectedFile();
+	            //This is where a real application would open the file.
+	            label.setText("Laduje " + file.toString());
+	        } else {
+	            label.setText("Error");
+	        }
 			
 		}
 		
