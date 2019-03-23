95bffbb8d4c750d7b1ac3683eee5fe692b701a80
bogdanpasterak <bogdanpasterak@gmail.com>
2019-03-23 22:30:52 +0000

    Rebuild Start

--- src/reportJUnitJava/Start.java
+++ src/reportJUnitJava/Start.java
@@ -1,10 +1,26 @@
 package reportJUnitJava;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class Start {
 
+	private static JFileChooser fileChooser;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		initFileChooser();
+	}
 
+	private static void initFileChooser() {
+		fileChooser = new JFileChooser(new File(System.getProperty("user.dir")));
+		fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("XML Files", "xml"));
+		fileChooser.setAcceptAllFileFilterUsed(false);
+		Integer returnVal = fileChooser.showOpenDialog(null);
+		File currentFile = fileChooser.getSelectedFile();
+
+		System.out.println(currentFile.getName());
 	}
 
 }
