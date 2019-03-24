3e1d688c46d14ecd4de5f9bf8494eab209df9657
bogdanpasterak <bogdanpasterak@gmail.com>
2019-03-24 19:17:18 +0000

    Read file

--- src/reportJUnitJava/Start.java
+++ src/reportJUnitJava/Start.java
@@ -7,20 +7,27 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Start {
 
+	File currentFile;
+	
 	private static JFileChooser fileChooser;
 	
+	private Start() {
+		initFileChooser();		
+		if (fileChooser.showOpenDialog(null) == 0 ) {
+			currentFile = fileChooser.getSelectedFile();
+
+			System.out.println(currentFile.getName());
+		}
+	}
+	
 	public static void main(String[] args) {
-		initFileChooser();
+		new Start();
 	}
 
 	private static void initFileChooser() {
 		fileChooser = new JFileChooser(new File(System.getProperty("user.dir")));
 		fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("XML Files", "xml"));
 		fileChooser.setAcceptAllFileFilterUsed(false);
-		Integer returnVal = fileChooser.showOpenDialog(null);
-		File currentFile = fileChooser.getSelectedFile();
-
-		System.out.println(currentFile.getName());
 	}
 
 }
